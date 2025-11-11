88def9956f30ba84277df6203687b84bcf193956
Knightec-Erik <erik.sjostedt@knightec.se>
2025-11-11 08:38:41 +0100

    more tests!

--- src/main.rs
+++ src/main.rs
@@ -503,8 +503,11 @@ fn exit() {
 // Tests
 #[cfg(test)]
 mod tests {
+    use crate::add;
     use crate::c_to_f;
     use crate::f_to_c;
+    use crate::mult;
+    use crate::sub;
 
     #[test]
     fn test_c_to_f() {
@@ -516,5 +519,23 @@ mod tests {
     fn test_f_to_c() {
         assert_eq!(f_to_c(32.0), 0.0);
         assert_eq!(f_to_c(55.5), 13.055556);
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(10, 10), 20);
+        assert_eq!(add(-5, 10), 5);
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(sub(5, 10), -5);
+        assert_eq!(sub(100, 100), 0);
+    }
+
+    #[test]
+    fn test_mult() {
+        assert_eq!(mult(7, 8), 56);
+        assert_eq!(mult(5, -10), -50);
     }
 }
